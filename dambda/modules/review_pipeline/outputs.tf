c038dea943ff05bcd82a877faefaf4b61acb049a
wjeong0530 <wjeong0530@gmail.com>
2026-08-06 16:51:07 +0900

    이것저것 또 합침

--- dambda/modules/review_pipeline/outputs.tf
+++ dambda/modules/review_pipeline/outputs.tf
@@ -2,6 +2,3 @@ output "queue_url" { value = aws_sqs_queue.review_moderation.url }
 output "queue_arn" { value = aws_sqs_queue.review_moderation.arn }
 output "dlq_arn" { value = aws_sqs_queue.dead_letter.arn }
 output "state_machine_arn" { value = aws_sfn_state_machine.review_moderation.arn }
-output "queue_arn" {
-  value = aws_sqs_queue.review_moderation.arn
-}
